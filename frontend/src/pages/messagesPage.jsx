0520bbbd5f6a8ffc856e9f2bbfc65ce47895f59b
Mihir Bairathi <mihirbairathi25@gmail.com>
2025-05-16 22:25:36 +0530

    some fixes

--- frontend/src/pages/messagesPage.jsx
+++ frontend/src/pages/messagesPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import arrowback from '../assets/arrowback.svg';
+// import arrowback from '../assets/arrowback.svg';
 import ProfileView from "../components/messages/ProfileView"
 import { connect } from 'react-redux';
 
