322aa89405bdbc0bf4e081e129f645fde4496e0e
Mihir Bairathi <mihirbairathi25@gmail.com>
2025-05-16 22:20:23 +0530

    some fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { configDotenv } from "dotenv"
+// import { configDotenv } from "dotenv"
 export const axiosInstance = axios.create({
     baseURL :import.meta.env.VITE_BASE_URL+"/api",
     withCredentials: true
