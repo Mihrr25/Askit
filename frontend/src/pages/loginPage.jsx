7ba092bd1b0e0bafbc72ff8e9fc92895a4f65947
Mihir Bairathi <mihirbairathi25@gmail.com>
2025-05-16 21:51:15 +0530

    some fixes

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import{useDispatch } from 'react-redux';
 import { useNavigate} from 'react-router-dom';
-import arrowback from '../assets/arrowback.svg';
+// import arrowback from '../assets/arrowback.svg';
 import {login} from "../actions/authActions"
 import {connect} from "react-redux"
 
