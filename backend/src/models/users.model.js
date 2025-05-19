a9009b79893339f83c07394ed646b58254db0731
Mihir Bairathi <mihirbairathi25@gmail.com>
2025-05-19 21:18:51 +0530

    some fixes

--- backend/src/models/users.model.js
+++ backend/src/models/users.model.js
@@ -9,6 +9,8 @@ const userSchema = new mongoose.Schema(
         },
         email:{
             type: String,
+            unique: true,
+            required: true,
         },
         password:{
             type: String,
