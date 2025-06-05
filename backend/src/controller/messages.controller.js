6c6cf8bdb9d44cd6a30e0361883417be3eeff8c9
Mihir Bairathi <mihirbairathi25@gmail.com>
2025-06-05 15:52:28 +0530

    some fixes

--- backend/src/controller/messages.controller.js
+++ backend/src/controller/messages.controller.js
@@ -116,7 +116,7 @@ export const getMessages = async (req, res) => {
         res.status(200).json(obj);
     } catch (error) {
         console.log("Error getting messages", error);
-        res.status(500).json({ message: "Internal Server Error",error: error.message,updated:"Now1" });
+        res.status(500).json({ message: "Internal Server Error",error: error.message,updated:"Now2" });
     }
 }
 
