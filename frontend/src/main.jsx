c64e260b2ee95affa3cce4036bb9ad41d2362cc9
Mihir Bairathi <mihirbairathi25@gmail.com>
2025-07-19 11:52:36 +0530

    added scrolling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,14 @@ import store from './lib/store.js'
 import { Provider } from 'react-redux'
 import { Toaster } from 'react-hot-toast'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registered.', reg))
+      .catch(err => console.log('Service Worker registration failed:', err));
+  });
+}
+
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <Toaster
