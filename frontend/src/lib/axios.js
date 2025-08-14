894ac31171da6d9f8ddfcac7949c03b297693b18
Mihir Bairathi <mihirbairathi25@gmail.com>
2025-08-14 20:40:56 +0530

    added fixes in icons

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { configDotenv } from "dotenv"
+// import { configDotenv } from "dotenv"
 export const axiosInstance = axios.create({
     baseURL :"/api",
     withCredentials: true
